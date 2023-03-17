8d00edcb0a141b88cabb2c044e1f34273d362e71
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-03-17 15:11:19 +0200

    RegEx Validator
    * add Validator using regular expressions

--- SPA/comments/forms.py
+++ SPA/comments/forms.py
@@ -1,8 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from captcha.fields import CaptchaField
+from django.contrib.auth.models import User
 from django import forms
+import re
 
-from .models import *
+from django.core.exceptions import ValidationError
+
+from .models import Comment, Profile
+
+
+def reg_validator(text):
+    tag_regex = re.compile(
+        r"<(?P<tag>(a|i|strong|code))\s*(?P<href>\s+href=(?P<q_mark>\"|\')[^\"<>]*(?P=q_mark))?(?P<title>\s+title=(?P<q_mark2>\"|\')[^\"<>]*(?P=q_mark2))?>[^<>]*</(?P=tag)>")
+    re_all = re.findall(tag_regex, text)
+    while re_all:
+        for re_obj in re_all:
+            if (re.search(r'href|title', re_obj[4]) or re.search(r'href|title', re_obj[4])) and re_obj[0] != 'a':
+                raise ValidationError('tags are wrong')
+        text = re.sub(tag_regex, '', text)
+        re_all = re.findall(tag_regex, text)
+
+    if '<' in text or '>' in text:
+        raise ValidationError('tags are wrong')
 
 
 class AddCommentForm(forms.ModelForm):
@@ -10,6 +29,11 @@ class AddCommentForm(forms.ModelForm):
                              widget=forms.ClearableFileInput(attrs={'multiple': False, 'accept': '.jpg, .png, .gif'}))
     file = forms.FileField(label='File', required=False,
                            widget=forms.ClearableFileInput(attrs={'multiple': False, 'accept': '.txt'}))
+
+    def clean_body(self):
+        body = self.cleaned_data['body']
+        reg_validator(body)
+        return body
 
     class Meta:
         model = Comment
